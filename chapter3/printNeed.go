a65387955ee37f41ceb36b0db467982b307a7b8a
KinChu <chuevgenii@gmail.com>
2023-07-29 23:07:18 +0300

    add example error processing

--- chapter3/printNeed.go
+++ chapter3/printNeed.go
@@ -1,26 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"math"
+)
 
-func paintNeeded(width float64, height float64) (float64, error){
-	if width < 0{
+func squareRoot(number float64) (float64, error) {
+	if number < 0 {
+		return 0, fmt.Errorf("can't get square root of negative number")
+	}
+	return math.Sqrt(number), nil
+}
+
+func paintNeeded(width float64, height float64) (float64, error) {
+	if width < 0 {
 		return 0, fmt.Errorf("a width of %0.2f is invalid", width)
 	}
-	if height < 0{
+	if height < 0 {
 		return 0, fmt.Errorf("a height of %0.2f is invalid", height)
 	}
 	area := width * height
-	return area/10.0, nil
+	return area / 10.0, nil
 }
 
-func main(){
+func main() {
 	var amount, total float64
-	amount, err := paintNeeded(4.2, -3.0)
-	fmt.Println(err)
+	amount, err := paintNeeded(4.2, 3.0)
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Printf("%0.2f liters needed\n", amount)
 	total += amount
-	amount, _ = paintNeeded(5.2, 3.5)
-	fmt.Printf("%0.2f liters needed\n", amount)
-	total += amount
+	amount, err = paintNeeded(5.2, 3.5)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("%0.2f liters needed\n", amount)
+		total += amount
+	}
 	fmt.Printf("Total: %0.2f liters\n", total)
+	root, err := squareRoot(-9.3)
+	if err != nil {
+		fmt.Println(err)
+	} else {		
+		fmt.Printf("%0.3f", root)
+	}
 }
