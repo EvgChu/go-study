57852486c805ad238101654170978cbf8bdb2df4
KinChu <chuevgenii@gmail.com>
2023-07-30 22:54:21 +0300

    add other example with pointer

--- chapter3/myIntPointer.go
+++ chapter3/myIntPointer.go
@@ -4,10 +4,23 @@ import (
 	"fmt"
 )
 
+func createPointer() *float64 {
+	var myFloat = 98.5 // in c this dont work
+	return &myFloat
+}
+
+func printPointer(myBoolPointer *bool) {
+	fmt.Println(*myBoolPointer)
+}
+
 func main() {
-	var myInt int 
+	var myInt int
 	var myIntPointer *int
 	myInt = 42
 	myIntPointer = &myInt
 	fmt.Println(*myIntPointer)
+	fmt.Println(*createPointer())
+	boolVar := true
+	printPointer(&boolVar)
+
 }
