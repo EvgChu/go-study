d0bc10ae7bac845e602e4413b14488e825addd38
KinChu <chuevgenii@gmail.com>
2023-08-08 09:00:13 +0300

    add godoc

--- usefulcmd.sh
+++ usefulcmd.sh
@@ -2,3 +2,4 @@ export GOPATH="/code" # set GOPATH="C:\code" for windows
 go doc github.com/EvgChu/keyboard
 go doc greeting AllGreetings # information about function in package
 go get github.com/EvgChu/keyboard
+godoc -http=:6060
