0680400fd4ecbcf809111094dc710ed78b44a482
KinChu <chuevgenii@gmail.com>
2023-08-16 21:09:02 +0300

    run example with delete

--- chapter7/task251.go
+++ chapter7/task251.go
@@ -4,7 +4,27 @@ import (
 	"fmt"
 )
 
-func main() {
+func exapmlePage252() {
+	var ok bool
+	ranks := make(map[string]int)
+	var rank int
+	ranks["bronze"] = 3
+	rank, ok = ranks["bronze"]
+	fmt.Printf("rank: %d, ok: %v\n", rank, ok)
+	delete(ranks, "bronze")
+	rank, ok = ranks["bronze"]
+	fmt.Printf("rank: %d, ok: %v\n", rank, ok)
+	isPrime := make(map[int]bool)
+	var prime bool
+	isPrime[5] = true
+	prime, ok = isPrime[5]
+	fmt.Printf("prime: %v, ok: %v\n", prime, ok)
+	delete(isPrime, 5)
+	prime, ok = isPrime[5]
+	fmt.Printf("prime: %v, ok: %v\n", prime, ok)
+}
+
+func taskPage251() {
 	data := []string{"a", "c", "e", "a", "e"}
 	counts := make(map[string]int)
 	for _, item := range data {
@@ -21,4 +41,10 @@ func main() {
 			fmt.Printf("%s: %d\n", letter, count)
 		}
 	}
+}
+
+func main() {
+	taskPage251()
+	fmt.Printf("--------------------\n")
+	exapmlePage252()
 }
