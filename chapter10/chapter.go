7ee56e8a61cf459cdc208c3d274b826bac03f7d7
KinChu <chuevgenii@gmail.com>
2023-08-25 23:43:03 +0300

    add another example

--- chapter10/chapter.go
+++ chapter10/chapter.go
@@ -38,6 +38,31 @@ func examplePage359() {
 	fmt.Println(value.MethodWithReturnValue())
 }
 
+type Whistle string
+
+func (w Whistle) MakeSound() {
+	fmt.Println("Tweet!")
+}
+
+type Horn string
+
+func (h Horn) MakeSound() {
+	fmt.Println("Honk!")
+}
+
+type NoiseMaker interface {
+	MakeSound()
+}
+
+func examplePage362() {
+	var toy NoiseMaker
+	toy = Whistle("Toyco Canary")
+	toy.MakeSound()
+	toy = Horn("Toyco Blaster")
+	toy.MakeSound()
+}
+
 func main() {
 	examplePage359()
+	examplePage362()
 }
