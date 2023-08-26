c169a705d54a5fcc1a491a80d65f3848f67a22fa
KinChu <chuevgenii@gmail.com>
2023-08-26 23:39:16 +0300

    add example with error

--- chapter10/chapter.go
+++ chapter10/chapter.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 )
 
 type MyInterface interface {
@@ -76,10 +77,36 @@ func examplePage369() {
 	noiseMaker.MakeSound()
 	var robot Robot = noiseMaker.(Robot)
 	robot.Walk()
+	horn, ok := noiseMaker.(Horn)
+	if ok {
+		horn.MakeSound()
+	}
+}
+
+type OverheatError float64
+
+func (o OverheatError) Error() string {
+	return fmt.Sprintf("Overheating by %0.2f degrees!", o)
+}
+
+func checkTemperature(actual float64, safe float64) error {
+	excess := actual - safe
+	if excess > 0 {
+		return OverheatError(excess)
+	}
+	return nil
+}
+
+func examplePage375() {
+	var err error = checkTemperature(21.379, 100.0)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
 	examplePage359()
 	examplePage362()
 	examplePage369()
+	examplePage375()
 }
