bd0c00ff652d39ea39f51ed921cbd653ae124a16
KinChu <chuevgenii@gmail.com>
2023-08-31 21:09:13 +0300

    add more tests

--- usefulcmd.sh
+++ usefulcmd.sh
@@ -3,3 +3,4 @@ go doc github.com/EvgChu/keyboard
 go doc greeting AllGreetings # information about function in package
 go get github.com/EvgChu/keyboard
 godoc -http=:6060
+go test  hfg/chapter12/prose -v
