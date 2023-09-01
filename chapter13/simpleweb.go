d73aae50bdb4dcd7588187aa63780090531db844
KinChu <chuevgenii@gmail.com>
2023-09-01 22:02:04 +0300

    var greeterFunction func()

--- chapter13/simpleweb.go
+++ chapter13/simpleweb.go
@@ -1,20 +1,52 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
-func viewHandler(writer http.ResponseWriter, request *http.Request) {
-	message := []byte("Hello, web!")
-	_, err := writer.Write(message)
+func sayHi() {
+	fmt.Println("Hi")
+}
+
+func divide(a int, b int) float64 {
+	return float64(a) / float64(b)
+}
+
+func exampleFuncVarible() {
+	var greeterFunction func()
+	var mathFunction func(int, int) float64
+	greeterFunction = sayHi
+	mathFunction = divide
+	greeterFunction()
+	fmt.Println(mathFunction(5, 2))
+}
+
+func write(writer http.ResponseWriter, message string) {
+	_, err := writer.Write([]byte(message))
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
+func viewHandler(writer http.ResponseWriter, request *http.Request) {
+	write(writer, "Hello, web!")
+}
+
+func frenchHandler(writer http.ResponseWriter, request *http.Request) {
+	write(writer, "Salut web!")
+}
+func hindiHandler(writer http.ResponseWriter, request *http.Request) {
+	write(writer, "Namaste, web!")
+}
+
 func main() {
+	exampleFuncVarible()
 	http.HandleFunc("/hello", viewHandler)
+	//todo «ListenAndServe», «Handler» and «ServeMux»
+	http.HandleFunc("/salut", frenchHandler)
+	http.HandleFunc("/namaste", hindiHandler)
 	err := http.ListenAndServe("localhost:8080", nil)
 	log.Fatal(err)
 }
