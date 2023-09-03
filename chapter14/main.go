7af9dd26d34f78aaf63b0a77a52ae2299e734a34
KinChu <chuevgenii@gmail.com>
2023-09-03 21:24:13 +0300

    finish simple site

--- chapter14/main.go
+++ chapter14/main.go
@@ -51,8 +51,29 @@ func viewHandler(writer http.ResponseWriter, request *http.Request) {
 	check(err)
 }
 
+func newHandler(writer http.ResponseWriter, request *http.Request) {
+	html, err := template.ParseFiles("chapter14/new.html")
+	check(err)
+	err = html.Execute(writer, nil)
+	check(err)
+}
+
+func createHandler(writer http.ResponseWriter, request *http.Request) {
+	signature := request.FormValue("signature")
+	options := os.O_WRONLY | os.O_APPEND | os.O_CREATE
+	file, err := os.OpenFile("chapter14/signatures.txt", options, os.FileMode(0600))
+	check(err)
+	_, err = fmt.Fprintln(file, signature)
+	check(err)
+	err = file.Close()
+	check(err)
+	http.Redirect(writer, request, "/guestbook", http.StatusFound)
+}
+
 func main() {
 	http.HandleFunc("/guestbook", viewHandler)
+	http.HandleFunc("/guestbook/new", newHandler)
+	http.HandleFunc("/guestbook/create", createHandler)
 	err := http.ListenAndServe("localhost:8080", nil)
 	log.Fatal(err)
 }
